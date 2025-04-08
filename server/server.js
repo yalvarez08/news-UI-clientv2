0c11fe5a853fb246aeabd61f00461d7f8113bb86
Yailene Alvarez <yailene.alvarez@gmail.com>
2025-04-08 19:28:18 -0400

    commit srvr folder

---
+++ server/server.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+const express = require('express');
+// const app = express();
+// const port = process.env.PORT || 5001;
+// const randomRouter = require('./routes/random.router');
+
+/** ---------- MIDDLEWARE ---------- **/
