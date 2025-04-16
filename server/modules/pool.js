5715139827a25cbead5e46a19ea987c9551223c2
Yailene Alvarez <yailene.alvarez@gmail.com>
2025-04-15 22:56:14 -0400

    modules added to svr folder

---
+++ server/modules/pool.js
@@ -0,0 +1,25 @@
+const pg = require('pg');
+let pool;
+
+if (process.env.DB_URL) {
+    pool = new pg.Pool({
+        connectionString: process.env.DB_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    });
+}
+
+else {
+    let databaseName = 'cmsc495_news_project'
+
+    if (process.env.NODE_ENV === 'test') {
+        databaseName = 'unit_testing'
+    }
+
+    pool = new pg.Pool({
+        host: 'localhost',
+        port: 5432,
+        database: databaseName,
+    });
+}
