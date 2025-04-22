b1be7828d3ca83176cb4b1a41b0fe24052204001
Yailene Alvarez <yailene.alvarez@gmail.com>
2025-04-22 01:38:53 -0400

    updated test files to cjs for compatibility

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-    testEnvironment: 'jsdom',
-    moduleNameMapper: {
-      '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-    },
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-  };
-  
