a2f6b5eb411f5d01de0e08bb2a0b63060653540c
Yailene Alvarez <yailene.alvarez@gmail.com>
2025-04-22 01:19:36 -0400

    babel and jest configuration

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+export default {
+    testEnvironment: 'jsdom',
+    moduleNameMapper: {
+      '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    },
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  };
+  
